171e6b8c1abff8ae605dc70e01f5a8292ef7fc22
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2017-08-03 19:07:49 +0300

    Remove unuseful example in functions lesson

--- lessons/05_functions/09_static_variable.php
+++ lessons/05_functions/09_static_variable.php
@@ -7,16 +7,3 @@ function funct() {
 }
 
 for ($i = 0; $i++<10;) funct();
-
-
-
-function test($a, $b, array $arr = []) {
-    // return 0;
-
-    $c = $a + $b;
-    return $c;
-}
-
-echo test(1, 2);
-
-
